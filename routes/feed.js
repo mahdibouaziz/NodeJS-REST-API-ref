dafd3cd56b7703950085b36a44e83ee27737b2f2
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-08-05 15:48:52 +0100

    add the PUT functionality

--- routes/feed.js
+++ routes/feed.js
@@ -18,6 +18,17 @@ router.post(
   feedController.createPost
 );
 
+// GET /feed/post/:postId
 router.get("/post/:postId", feedController.getPost);
+
+// PUT /feed/post/:postId
+router.put(
+  "/post/:postId",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
 
 module.exports = router;
