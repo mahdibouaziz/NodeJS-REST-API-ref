42271baa28539a5d5259989917c02b4444a763e1
mahdibouaziz <mahdibouaziz@hotmail.fr>
2021-08-06 15:40:30 +0100

    added the isAuth to all routes thet need auth

--- routes/feed.js
+++ routes/feed.js
@@ -16,11 +16,12 @@ router.post(
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   feedController.createPost
 );
 
 // GET /feed/post/:postId
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
 // PUT /feed/post/:postId
 router.put(
@@ -29,10 +30,11 @@ router.put(
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
   ],
+  isAuth,
   feedController.updatePost
 );
 
 // DELETE /feed/post/:postId
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
